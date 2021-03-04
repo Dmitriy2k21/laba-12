ff646d20a95451877f4ba8b97fd949ed0eae8bfc
Dmitriy2k21 <uliaalekseevaoff@mail.ru>
2021-03-04 11:28:57 +0700

    Отформатирован код

--- Unit1.dfm
+++ Unit1.dfm
@@ -2,7 +2,7 @@ object Form1: TForm1
   Left = 0
   Top = 0
   Caption = 'Form1'
-  ClientHeight = 299
+  ClientHeight = 410
   ClientWidth = 635
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
@@ -14,31 +14,46 @@ object Form1: TForm1
   PixelsPerInch = 96
   TextHeight = 13
   object Button1: TButton
-    Left = 8
+    Left = 16
     Top = 8
-    Width = 401
-    Height = 105
-    Caption = #1057#1086#1079#1076#1072#1090#1100' '#1086#1073#1098#1077#1082#1090' '#1082#1083#1072#1089#1089#1072' TParallel '
-    Font.Charset = DEFAULT_CHARSET
-    Font.Color = clWindowText
-    Font.Height = -24
-    Font.Name = 'Tahoma'
-    Font.Style = []
-    ParentFont = False
+    Width = 241
+    Height = 57
+    Caption = #1057#1086#1079#1076#1072#1090#1100' '#1086#1073#1098#1077#1082#1090' '#1082#1083#1072#1089#1089#1072' TParallel'
     TabOrder = 0
+    OnClick = Button1Click
   end
   object Button2: TButton
-    Left = 8
-    Top = 119
-    Width = 401
-    Height = 105
+    Left = 16
+    Top = 71
+    Width = 241
+    Height = 65
     Caption = #1055#1088#1086#1074#1077#1088#1080#1090#1100' '#1089#1091#1097#1077#1089#1090#1074#1091#1077#1090' '#1083#1080' '#1086#1073#1098#1077#1082#1090' '#1074' '#1087#1072#1084#1103#1090#1080
-    Font.Charset = DEFAULT_CHARSET
-    Font.Color = clWindowText
-    Font.Height = -19
-    Font.Name = 'Tahoma'
-    Font.Style = []
-    ParentFont = False
     TabOrder = 1
+    OnClick = Button2Click
+  end
+  object Memo1: TMemo
+    Left = 16
+    Top = 142
+    Width = 241
+    Height = 122
+    TabOrder = 2
+  end
+  object Button3: TButton
+    Left = 16
+    Top = 270
+    Width = 241
+    Height = 58
+    Caption = #1042#1099#1074#1077#1089#1090#1080' '#1079#1085#1072#1095#1077#1085#1080#1103' '#1087#1086#1083#1077#1081' '#1084#1072#1089#1089#1080#1074#1072
+    TabOrder = 3
+    OnClick = Button3Click
+  end
+  object Button4: TButton
+    Left = 16
+    Top = 334
+    Width = 241
+    Height = 59
+    Caption = #1057#1086#1079#1076#1072#1090#1100' '#1086#1073#1098#1077#1082#1090' '#1082#1083#1072#1089#1089#1072' TBar = class(TParallel)'
+    TabOrder = 4
+    OnClick = Button4Click
   end
 end
--- Unit1.pas
+++ Unit1.pas
@@ -3,74 +3,137 @@ unit Unit1;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
   TForm1 = class(TForm)
     Button1: TButton;
     Button2: TButton;
+    Memo1: TMemo;
+    Button3: TButton;
+    Button4: TButton;
     procedure Button2Click(Sender: TObject);
     procedure Button1Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
   private
     { Private declarations }
   public
     { Public declarations }
   end;
+
 type
-TParallel = class
+  TParallel = class
   public
-  Fa:word;
-  Fb:word;
-  Fc:word;
-  procedure Init(a,b,c:word);
-  function Volume: word;
-  procedure Show;
+    Fa: word;
+    Fb: word;
+    Fc: word;
+    constructor Create(a, b, c: word);
+    function Volume: word;
+    procedure Show;
   end;
+
+  TBar = class(TParallel)
+  public
+    FRo: real;
+    constructor Create(a, b, c: word; Ro: real);
+    function massa: real;
+    procedure Show;
+  end;
+
 var
   Form1: TForm1;
-  Par1:TParallel;
+  Par1: TParallel;
+  Par: array [1 .. 5] of TParallel;
+  x: word = 0;
+  y: word = 0;
+  z: word = 0;
+  Bar1: TBar;
 
 implementation
-  { TParallel }
-// Метод инициализирующий поля объекта класса TParallel
-// Метод, отображающий значение объема, а также всех полей Fa, Fb и F
-// Метод, возвращающий объем
-// Метод, инициализирующий поля объекта класса TParallel
-procedure TParallel.Init(a, b, c: word);
+
+{$R *.dfm}
+{ TParallel }
+
+constructor TParallel.Create(a, b, c: word);
 begin
-Fa:=a; //присваиваем полю Fa значение a
-Fb:=b; //присваиваем полю Fb значение b
-Fc:=c; //присваиваем полю Fc значение c
+  Fa := a;
+  Fb := b;
+  Fc := c;
 end;
-// Метод, отображающий значение объема, а также всех полей Fa, Fb и Fc
+
 procedure TParallel.Show;
 begin
-ShowMessage('Объем параллелепипеда равен ' + IntToStr(Volume)+#10#13+
-'Ширина – Поле Fa= '+IntToStr(Fa)+#10#13+
-'Длина – Поле Fb= '+IntToStr(Fb) +#10#13+
-'Высота – Поле Fc= '+IntToStr(Fc) );
+  ShowMessage('Объём параллелепипеда равен ' + IntToStr(Volume) + #10#13 +
+    'Ширина - Поле Fa= ' + IntToStr(Fa) + #10#13 + 'Длина - Поле Fb= ' +
+    IntToStr(Fb) + #10#13 + 'Высота - Поле Fc= ' + IntToStr(Fc));
 end;
-// Метод, возвращающий объем
-function TParallel.Volume:word;
+
+function TParallel.Volume: word;
 begin
-result:=Fa*Fb*Fc; {возвращаем во внешнюю программу объем в виде значения типа word}
+  result := Fa * Fb * Fc;
 end;
-{$R *.dfm}
 
 procedure TForm1.Button1Click(Sender: TObject);
 begin
-Par1:=TParallel.Create; // Создать объект в памяти
-Par1.Init(1,2,3); // Инициализировать поля объекта
-Par1.Show; // Вывести на экран значение объема и полей объекта
+  x := x + 1;
+  y := y + 1;
+  z := z + 1;
+  Par1 := TParallel.Create(x, y, z);
+  Par[x] := Par1;
+  Par1.Show;
+  Memo1.Lines.Add('Адрес в памяти объекта, содержащийся в Par1, равен ' +
+    IntToStr(integer(Par1)));
 end;
 
 procedure TForm1.Button2Click(Sender: TObject);
 begin
-if Par1=nil // если Par1 ни на что не указывает
-then // то
-Form1.Caption:='Объекта в памяти нет'
-else Form1.Caption:= 'Объект существует в памяти';
+  if Par1 = nil then
+    Form1.Caption := 'Объекта в памяти нет'
+  else
+    Form1.Caption := 'Объект существует в памяти';
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+var
+  i: integer;
+begin
+  for i := 1 to 5 do
+  begin
+    Memo1.Lines.Add('Адрес объекта с номером ' + IntToStr(i) + ' равен ' +
+      IntToStr(integer(Par[i])) + ' Поле Fa = ' + IntToStr(Par[i].Fa));
+    Par[i].Show;
+  end;
+end;
+
+procedure TForm1.Button4Click(Sender: TObject);
+begin
+  Bar1 := TBar.Create(1, 2, 3, 10.5);
+  Bar1.Show;
+end;
+
+{ TBar }
+
+constructor TBar.Create(a, b, c: word; Ro: real);
+begin
+  inherited Create(a, b, c);
+  FRo := Ro;
+end;
+
+function TBar.massa: real;
+begin
+  result := FRo * Volume;
+end;
+
+procedure TBar.Show;
+begin
+  ShowMessage('Объем параллелепипеда равен ' + IntToStr(Volume) + #10#13 +
+    'Ширина – Поле Fa= ' + IntToStr(Fa) + #10#13 + 'Длина – Поле Fb= ' +
+    IntToStr(Fb) + #10#13 + 'Высота – Поле Fc= ' + IntToStr(Fc) + #10#13 +
+    'Плотность – Поле FRo= ' + FloatToStr(FRo) + #10#13 + 'Масса = ' +
+    FloatToStr(massa));
 end;
 
 end.
